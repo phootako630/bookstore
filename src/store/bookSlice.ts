38705a7fef64eacbd0d21be65ab2e67dd4cd4983
ivanzhang <ivanzhang220820@gmail.com>
2023-09-15 22:20:46 -0400

    Change id to string

--- src/store/bookSlice.ts
+++ src/store/bookSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface Book {
-    id: number;
+    id: string;
     name: string;
     price: number;
     category: string;
@@ -10,14 +10,14 @@ export interface Book {
 
 const initialState: Book[] = [
     {
-        id: 1,
+        id: "1",
         name: 'The Great Gatsby',
         price: 10.99,
         category: 'Fiction',
         description: 'A classic novel about the American dream.'
     },
     {
-        id: 2,
+        id: "2",
         name: 'Moby Dick',
         price: 12.99,
         category: 'Adventure',
